cc38e0b74c86b612b24e322a830412dcf8936a55
Fauzer Santos <fauzer.dev@gmail.com>
2023-02-21 09:55:32 -0400

    Aula 3
    sobre Strings

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Fauzer Santos')
+print(1, 'Fauzer "Santos"')
+
+# Aspas duplas
+print("Fauzer Santos")
+print(2, "Fauzer 'Santos'")
+
+# Escape
+print("Fauzer \"Santos\"")
+
+# r
+print(r"Fauzer \"Santos\"")
