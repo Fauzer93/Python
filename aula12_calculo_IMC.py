dd89fe13b2658e24bfcc71b79364307f94393e8a
Fauzer Santos <fauzer.dev@gmail.com>
2023-03-11 16:45:15 -0400

    aula 12

---
+++ aula12_calculo_IMC.py
@@ -0,0 +1,11 @@
+nome = 'Fauzer Santos'
+altura = 1.81
+peso = 72
+imc = peso / (altura * altura)
+print('{}tem {} de altura,'.format(nome, altura))
+print('pesa {} quilos e seu IMC é'.format(peso))
+print('{}'.format(imc))
+
+# Fauzer SAntos tem 1.81 de altura
+# pesa 72 quilos e seu IMC é
+# 21.98
