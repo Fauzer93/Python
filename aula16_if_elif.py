2994f18e81588c1bb70891e72d7d642f04d74556
Fauzer Santos <fauzer.dev@gmail.com>
2023-03-23 04:05:21 -0400

    aula 16
    Sobre If, Elif e Else

---
+++ aula16_if_elif.py
@@ -0,0 +1,13 @@
+# if / se
+# elif / se não se
+# else / se não  
+entrada = input('Você quer "Entrar" ou "Sair"? ')
+
+if entrada == 'Entrar':
+    print('Você entrou no sistema')
+
+elif  entrada == 'Sair':
+    print('Você saiu do sistema')
+
+else:
+    print('Você não digitou nem entrar e nem sair.')
