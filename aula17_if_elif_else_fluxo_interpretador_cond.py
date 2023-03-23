aa3a4baf67b791019ff661727bdaba47a9657a83
Fauzer Santos <fauzer.dev@gmail.com>
2023-03-23 04:05:52 -0400

    Aula 17
    Interpretador de condições do If, Elif e Else

---
+++ aula17_if_elif_else_fluxo_interpretador_cond.py
@@ -0,0 +1,23 @@
+# if / se
+# elif / se não se
+# else / se não  
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Código da condição 1')
+elif condicao2:
+    print('Código da condição 2')
+elif condicao3:
+    print('Código da condição 3')
+elif condicao4:
+    print('Código da condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
