cbe5b9c55d614ab6edcf2cd5c601d352cfefb03d
Fauzer Santos <fauzer.dev@gmail.com>
2023-03-25 16:51:10 -0400

    aula 23
    Sobre operador NOT

---
+++ aula23_operador_NOT.py
@@ -0,0 +1,14 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+senha = input('Senha: ')
+
+if senha == '123456':
+    print('Entrou')
+else:
+    print('Senha Incorreta')
+
+print(not True)  # False
+print(not False)  # True
