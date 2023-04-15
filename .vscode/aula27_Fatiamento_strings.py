a46c5b42d55d5d9480c0902bfd9bf96d19c2b6a9
Fauzer Santos <fauzer.dev@gmail.com>
2023-04-15 10:53:16 -0400

    Aula 27
    Fatiamento de Strings

---
+++ .vscode/aula27_Fatiamento_strings.py
@@ -0,0 +1,13 @@
+"""
+Fatiamento de strings
+012345678
+Olá mundo
+987654321
+Fatimaneto [i:f:p][::]
+Obs.: a função len retorna a qts
+de caracteres da str
+"""
+
+varialvel = 'Olá mundo'
+print(varialvel[4:])
+print(len(varialvel[4:]))
