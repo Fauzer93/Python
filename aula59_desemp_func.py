346a9e7e817a187e9197f62903fe3fe814d731eb
Fauzer Santos <fauzer.dev@gmail.com>
2023-06-29 12:08:59 -0400

    Aulas finais iniciante

---
+++ aula59_desemp_func.py
@@ -0,0 +1,9 @@
+string = 'ABCD'
+lista = ['Church', 'jose',1, 2, 3, 'maria',]
+tupla = 'python', 'é', 'legal'
+
+#p, b, *_, u = lista
+#print(p, u)
+
+for nome in lista:
+    print(nome, end=' ')
---
+++ aula60_ope_ternaria.py
@@ -0,0 +1,16 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+"""
+
+
+#condicao = 10 == 101
+#variavel = 'valor' if condicao else 'outro valor'
+#print(variavel)
+
+#digito = 1 # > 9 = 0
+#novo_digito = digito if digito <= 9 else 0
+#novo_digito = 0 if digito > 9 else digito
+#print(novo_digito)
+
+print('valor' if True else 'outro valor' if True else 'fim')
