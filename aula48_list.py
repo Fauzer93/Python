cc2204a6cf6223dc9f70d04952415a980842cb56
Fauzer Santos <fauzer.dev@gmail.com>
2023-06-28 20:57:03 -0400

    Aula 48
    Sobre list no python

---
+++ aula48_list.py
@@ -0,0 +1,22 @@
+#string = 'ABCDE'# 5 caracteres (len)
+
+# print(bool([])) # false
+# print(lista, type(lista))
+
+#..........0...1.........2...........3....4
+#..........-5..-4.......-3...........-2...-1
+#lista = [123, True, 'fauzer santos', 1.2, []]
+#lista[-3] = 'adriano'
+#print(lista)
+#print(lista[2], type(lista[2]))
+
+
+lista = [10, 20, 30, 40]
+#lista[2] = 300
+#del lista[2]
+#print(lista)
+#print(lista[2])
+
+lista.append(50)
+lista.append(60)
+lista.append(70)
