89d8378df979dfce7ca538dddb6477b86e568da8
Fauzer Santos <fauzer.dev@gmail.com>
2023-06-29 07:44:18 -0400

    Aulas

---
+++ aula57_lista_inte.py
@@ -0,0 +1,16 @@
+"""
+lista de listas e seus índices
+"""
+
+salas = [
+    # 0         1
+    ['Fauzer', 'Santos',],  # 0
+    # 0
+    ['Adriano',], # 1
+    # 0         1       2
+    ['Church', 'jose', 'maria',], # 2
+
+]
+
+
+print(salas[2][2])
