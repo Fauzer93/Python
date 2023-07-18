c130005a9714d6fb0012b58b7bd1b40db968fb77
Fauzer Santos <fauzer.dev@gmail.com>
2023-07-17 20:19:35 -0400

    Aula 66
    Argumentos em python

---
+++ aula66_nome_arg.py
@@ -0,0 +1,17 @@
+"""
+Argumentos nomeados e nomeados em funções python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
+
+
+def soma(x, y, z):
+    #Definição
+    print(f'{x=} y={y} {z=}', '|', 'x + y + z = ', x + y + z)
+
+
+soma(1, 2, 3)
+soma(1, y=2, z=5)
+
+
+print(1, 2, 3, sep='-')
